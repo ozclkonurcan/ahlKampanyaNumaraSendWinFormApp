508854bee334782ed643aa30c2ddae97c4eacead
ozclkonurcan <71335949+ozclkonurcan@users.noreply.github.com>
2022-11-21 17:47:36 +0300

    :D

---
+++ windows form app/KarelSeviceFormApp/KarelSeviceFormApp/VM_Model/VM_ApiGetCampaigns.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarelSeviceFormApp.VM_Model
+{
+    public class VM_ApiGetCampaigns
+    {
+        public int CampID { get; set; }
+        public string campName { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string status { get; set; }
+    }
+}
