17601e4423bb0ebcf0b6395b8a282ae0d6e75c6f
ozclkonurcan <71335949+ozclkonurcan@users.noreply.github.com>
2022-11-23 17:19:26 +0300

    20221123

---
+++ WinForm20221123/KarelSeviceFormApp/KarelSeviceFormApp/VM_Model/VM_ApiGetPoll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarelSeviceFormApp.VM_Model
+{
+    public class VM_ApiGetPoll
+    {
+        public int pollID { get; set; }
+        public string pollName { get; set; }
+        public string pollStartDate { get; set; }
+        public string pollEndDate { get; set; }
+        public string pollLang { get; set; }
+        public int pollStatus { get; set; }
+    }
+}
